7cdb25c07780294602c30471f9f4641ba0a1707b
IM23a-tutkuno <tutkuno@bzz.ch>
2025-10-28 10:18:28 +0100

    added database mapping

--- src/main/java/ch/bzz/Account.java
+++ src/main/java/ch/bzz/Account.java
@@ -1,15 +1,31 @@
 package ch.bzz;
 
+import ch.bzz.Project;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "account")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "accountNumber", nullable = false, length = 10)
     private String accountNumber;
+
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
-    private Project project;  // Beziehung zu Project
+
+    @ManyToOne
+    @JoinColumn(name = "projectName", nullable = false)
+    private Project project;
 }
--- src/main/java/ch/bzz/Booking.java
+++ src/main/java/ch/bzz/Booking.java
@@ -1,20 +1,44 @@
 package ch.bzz;
 
+import jakarta.persistence.*;
+import ch.bzz.Account;
+import ch.bzz.Project;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "booking")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "date", nullable = false)
     private LocalDate date;
+
+    @Column(name = "text", nullable = false, length = 255)
     private String text;
-    private Account debitAccount;   // Soll-Konto
-    private Account creditAccount;  // Haben-Konto
+
+    @Column(name = "amount", nullable = false)
     private double amount;
-    private Project project;        // Bezug zu Project
+
+    @ManyToOne
+    @JoinColumn(name = "debitAccountId", nullable = false)
+    private Account debitAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "creditAccountId", nullable = false)
+    private Account creditAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "projectName", nullable = false)
+    private Project project;
 }
