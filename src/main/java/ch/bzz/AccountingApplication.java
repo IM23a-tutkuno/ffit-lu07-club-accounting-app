94bedf519938b3ff216e81e3ebc147db08995f73
IM23a-tutkuno <tutkuno@bzz.ch>
2025-11-18 09:02:32 +0100

    added cors and json nullable

--- src/main/java/ch/bzz/AccountingApplication.java
+++ src/main/java/ch/bzz/AccountingApplication.java
@@ -1,11 +1,40 @@
 package ch.bzz;
 
+import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AccountingApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountingApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(false)
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonNullableModule() {
+        return builder -> builder.modulesToInstall(JsonNullableModule.class);
+    }
+
 }
